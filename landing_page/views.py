252da764365600771f48636f2969cbb8ee9fe2c2
Jacob <jacobshnn@gmail.com>
2018-03-22 22:26:10 -0700

    Updates with site working

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from .models import landing_page_header, landing_page_home
 
 # Create your views here.
 def index(request):
     # Render the HTML template index.html with the data in the context variable
+    test = landing_page_header.objects.all()
+
+    for o in test:
+        print(o.pk)
+
     return render(
         request,
-        'base_generic.html',
-        context={}
+        'index.html',
+        context={'header_title': test[0].name}
     )
