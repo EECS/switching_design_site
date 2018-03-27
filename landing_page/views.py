32ce57313e122e95d9b2093a417f173dc1529da6
Shannon <Jacob.Shannon@craneae.com>
2018-03-27 09:32:06 -0700

    Example design populated in landing page

--- landing_page/views.py
+++ landing_page/views.py
@@ -13,6 +13,17 @@ def index(request):
     show_ana_electronics = False
     show_dig_electronics = True
 
+    input_voltage = 24
+    output_voltage = 5
+    output_current = 3
+    q1_on_res = 25 #milliOhms
+    d1_on_volt = 0.7
+    inductor_res = 5 #milliohms
+    capacitor_res = 10 #milliohms
+    inductance = 10 #microhenries
+    capacitance = 100 #microfarads
+    load_res = 1.67 #ohms
+
     return render(
         request,
         'index.html',
@@ -23,5 +34,14 @@ def index(request):
                 'paid_site':paid_site,
                 'show_power_electronics':show_power_electronics,
                 'show_ana_electronics':show_ana_electronics,
-                'show_dig_electronics':show_dig_electronics}
+                'show_dig_electronics':show_dig_electronics,
+                'input_voltage':input_voltage,
+                'output_voltage':output_voltage,
+                'output_current':output_current,
+                'q1_on_res':q1_on_res,
+                'd1_on_volt':d1_on_volt,
+                'inductor_res':inductor_res,
+                'capacitor_res':capacitor_res,
+                'inductance':inductance,
+                'capacitance':capacitance}
     )
