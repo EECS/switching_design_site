92dad42eeb3cafdb3d7a9f02973005b14385d627
Shannon <Jacob.Shannon@craneae.com>
2018-03-26 17:42:06 -0700

    Visual Website created, models simplified

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from .models import landing_page_header, landing_page_home
 
 # Create your views here.
 def index(request):
     # Render the HTML template index.html with the data in the context variable
-    test = landing_page_header.objects.all()
-
-    for o in test:
-        print(o.pk)
+    show_testimonials = False
+    trial_length = 14
+    header_title = "Design Electronics"
+    tag_break_lines = range(10)
 
     return render(
         request,
         'index.html',
-        context={'header_title': test[0].name}
+        context={'header_title': header_title,
+                'show_testimonials': show_testimonials,
+                'trial_length':trial_length,
+                'tag_break_lines': tag_break_lines}
     )
